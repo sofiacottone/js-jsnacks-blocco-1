a6f4f4e908a2dbccc67a4601b59afb28f00418b1
sofiacottone <cottonesofia@outlook.it>
2024-03-11 15:36:08 +0100

    feat: add js logic

--- jsnack-1/js/script.js
+++ jsnack-1/js/script.js
@@ -1,15 +1,15 @@
 // Chiedere all'utente 5 numeri. 
 // Inserire i numeri nell'array ma senza creare duplicati(inserire il numero nell'array solo se non è già presente).
 
-// chiedo i 5 numeri all'utente 
 const numbers = [];
 
 for (let i = 0; i < 5; i++) {
+    // chiedo i 5 numeri all'utente 
     let userNumber = parseInt(prompt('Dimmi un numero'));
     console.log(userNumber);
-
+    // verifico se il numero inserito dall'utente è presente nell'array
     let numberFound = numbers.includes(userNumber);
-
+    // se non è presente allora lo inserisco
     if (numberFound === false) {
         numbers.push(userNumber);
     }
