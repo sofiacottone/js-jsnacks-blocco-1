b356a5ca76e4bc57ab81e934a5e7970a901d7db1
sofiacottone <cottonesofia@outlook.it>
2024-03-11 16:21:16 +0100

    feat: add jsnack 4 logic

--- jsnack-4/js/script.js
+++ jsnack-4/js/script.js
@@ -1,2 +1,18 @@
 // Calcola la somma e la media dei primi 10 numeri.
 
+// creo un array con 10 numeri
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// calcolo la somma
+let sum = 0;
+
+for (i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+}
+
+console.log(sum);
+
+// calcolo la media
+const average = sum / numbers.length;
+
+console.log(average);
