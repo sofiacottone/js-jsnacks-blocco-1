bb8e0b5f9af149d5c6bb267a107ec8a02b92c009
sofiacottone <cottonesofia@outlook.it>
2024-03-11 15:58:12 +0100

    feat: add jsnack 3 logic

--- jsnack-3/js/script.js
+++ jsnack-3/js/script.js
@@ -1,3 +1,20 @@
 // Crea un array vuoto.
 // Chiedi per 6 volte all’utente di inserire un numero
 // se è dispari inseriscilo nell’array.
+
+// creo un array vuoto
+const oddNumbers = [];
+
+for (i = 0; i < 6; i++) {
+
+    // chiedo per 6 volte all’utente di inserire un numero
+    let userNumber = parseInt(prompt('Inserisci un numero'));
+    console.log('user number', userNumber);
+
+    // se è dispari lo inserisco nell'array
+    if (userNumber % 2 !== 0) {
+        oddNumbers.push(userNumber);
+    }
+
+    console.log('final', oddNumbers);
+}
